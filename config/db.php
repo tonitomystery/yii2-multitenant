d2d949b385b202ab1d1e4906e85191857b7376ec
tonitomystery <amaristany01@gmail.com>
2026-07-17 22:04:29 -0400

    feat: add ignore and config controller

--- config/db.php
+++ config/db.php
@@ -2,9 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => getenv("DB_DSN"),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
